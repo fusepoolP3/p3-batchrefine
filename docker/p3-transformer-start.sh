5b25b1312fa0975e4667d4eff52c4cf88452d343
gmega <mega@static.148-251-20-98.clients.your-server.de>
2014-09-25 10:15:31 +0200

    - fix shebang and paths to start script.

--- docker/p3-transformer-start.sh
+++ docker/p3-transformer-start.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Starts OpenRefine ...
-../OpenRefine/refine &
+/OpenRefine/refine &
 
 # ... and the P3 transformer.
-java -jar ../clients/clients-transformer/target/clients-transformer-0.1-jar-with-dependencies.jar -t async
+java -jar /batchrefine/clients/clients-transformer/target/clients-transformer-0.1-jar-with-dependencies.jar -t async
