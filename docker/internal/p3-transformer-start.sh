4e5559d992d9d099a725efa3947d78b6c7b24ffe
gmega <mega@hetzy1.spaziodati.eu>
2014-11-11 18:57:19 +0100

    - give more memory to docker refine by default.

--- docker/internal/p3-transformer-start.sh
+++ docker/internal/p3-transformer-start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Starts OpenRefine ...
-/OpenRefine/refine &
+/OpenRefine/refine -m 8096M&
 
 # ... and the P3 transformer.
 java -jar /batchrefine/clients/clients-transformer/target/clients-transformer-1.0.0-SNAPSHOT-jar-with-dependencies.jar -t async
