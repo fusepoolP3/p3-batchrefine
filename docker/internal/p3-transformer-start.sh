308829e82529f75b311a455483f67e15562ad7ac
gmega <mega@hetzy1.spaziodati.eu>
2014-11-11 12:05:49 +0100

    - Fix dockerfile/script to accomodate change in repo name and version (need to find a better way to do this).

--- docker/internal/p3-transformer-start.sh
+++ docker/internal/p3-transformer-start.sh
@@ -4,4 +4,4 @@
 /OpenRefine/refine &
 
 # ... and the P3 transformer.
-java -jar /batchrefine/clients/clients-transformer/target/clients-transformer-0.1-jar-with-dependencies.jar -t async
+java -jar /batchrefine/clients/clients-transformer/target/clients-transformer-1.0.0-SNAPSHOT-jar-with-dependencies.jar -t async
