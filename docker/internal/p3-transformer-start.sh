c274fac0c2ded4ac03748177db553a0b496bf076
gmega <mega@spaziodati.eu>
2014-11-12 15:41:09 +0100

    - fix bug to send OpenRefine to background on docker run

--- docker/internal/p3-transformer-start.sh
+++ docker/internal/p3-transformer-start.sh
@@ -7,7 +7,7 @@ echo "refine options are ${REFINE_OPTIONS}"
 echo "transformer options are ${TRANSFORMER_OPTIONS}"
 
 # Starts OpenRefine ...
-/OpenRefine/refine ${REFINE_OPTIONS}
+/OpenRefine/refine ${REFINE_OPTIONS} &
 
 # ... and the P3 transformer.
-java -jar /batchrefine/clients/clients-transformer/target/clients-transformer-0.1-jar-with-dependencies.jar ${TRANSFORMER_OPTIONS}
+java -jar /batchrefine/clients/clients-transformer/target/clients-transformer-1.0.0-SNAPSHOT-jar-with-dependencies.jar ${TRANSFORMER_OPTIONS}
