018123be34c48af04dbd8ee991924b6d52426acf
gmega <mega@spaziodati.eu>
2014-11-12 15:29:54 +0100

    - merge transformer script

--- docker/internal/p3-transformer-start.sh
+++ docker/internal/p3-transformer-start.sh
@@ -7,14 +7,7 @@ echo "refine options are ${REFINE_OPTIONS}"
 echo "transformer options are ${TRANSFORMER_OPTIONS}"
 
 # Starts OpenRefine ...
-<<<<<<< HEAD
 /OpenRefine/refine ${REFINE_OPTIONS}
 
 # ... and the P3 transformer.
 java -jar /batchrefine/clients/clients-transformer/target/clients-transformer-0.1-jar-with-dependencies.jar ${TRANSFORMER_OPTIONS}
-=======
-/OpenRefine/refine -m 8096M&
-
-# ... and the P3 transformer.
-java -jar /batchrefine/clients/clients-transformer/target/clients-transformer-1.0.0-SNAPSHOT-jar-with-dependencies.jar -t async
->>>>>>> 4e5559d992d9d099a725efa3947d78b6c7b24ffe
