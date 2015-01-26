529ee064e070004e08943306f825daeb08f0df2f
gmega <mega@hetzy1.spaziodati.eu>
2015-01-26 11:47:12 +0100

    - Fix parameter handling with transformer script to allow it to take more than one parameter.

--- docker/internal/p3-transformer-start.sh
+++ docker/internal/p3-transformer-start.sh
@@ -4,8 +4,9 @@ BATCHREFINE_ROOT=${BATCHREFINE_ROOT:-/batchrefine}
 OPENREFINE_ROOT=${OPENREFINE_ROOT:-/OpenRefine}
 
 REFINE_OPTIONS=${1:-''}
-TRANSFORMER_OPTIONS=${2:-''}
 VERSION=`cat ${BATCHREFINE_ROOT}/version`
+
+shift 1
 
 echo "refine options are ${REFINE_OPTIONS}"
 echo "transformer options are ${TRANSFORMER_OPTIONS}"
@@ -14,4 +15,4 @@ echo "transformer options are ${TRANSFORMER_OPTIONS}"
 ${OPENREFINE_ROOT}/refine ${REFINE_OPTIONS} &
 
 # ... and the P3 transformer.
-java -jar ${BATCHREFINE_ROOT}/clients/clients-transformer/target/clients-transformer-${VERSION}-jar-with-dependencies.jar ${TRANSFORMER_OPTIONS}
+java -jar ${BATCHREFINE_ROOT}/clients/clients-transformer/target/clients-transformer-${VERSION}-jar-with-dependencies.jar "$@"
