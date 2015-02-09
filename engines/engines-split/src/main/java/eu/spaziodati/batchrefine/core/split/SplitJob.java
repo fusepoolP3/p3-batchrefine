85220ebdee45a650fcd8dcd3d0ad992c9445c6fb
Andrey Bratus <bratus@spaziodati.eu>
2015-02-09 18:03:40 +0100

    merge with development track

---
+++ engines/engines-split/src/main/java/eu/spaziodati/batchrefine/core/split/SplitJob.java
@@ -0,0 +1,136 @@
+package eu.spaziodati.batchrefine.core.split;
+
+import eu.spaziodati.batchrefine.core.IAsyncTransformEngine.Callback;
+import eu.spaziodati.batchrefine.core.MultiInstanceEngine;
+import eu.spaziodati.batchrefine.core.PartFilesReassembly;
+import eu.spaziodati.batchrefine.core.split.SplitLogicFactory.ISplitLogic;
+import eu.spaziodati.batchrefine.core.split.SplitLogicFactory.ISplitLogic.TransformTask;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang.RandomStringUtils;
+import org.json.JSONArray;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Properties;
+
+public class SplitJob extends Callback {
+
+    private static final int IDENTIFIER_LENGTH = 10;
+
+    private File inputFile;
+    private File outputFile;
+    private JSONArray transform;
+    private File tempDir;
+    private int id;
+    private ISplitLogic splitLogic;
+    private ArrayList<TransformTask> chunks;
+    private Properties exporterProperties;
+    @SuppressWarnings("unused")
+    private volatile int taskCounter;
+
+    public SplitJob(int idNumber) {
+        id = idNumber;
+    }
+
+    public SplitJob configure(URI original, JSONArray transform,
+                              URI transformed, Properties jobConfig) throws Exception {
+        inputFile = new File(original);
+        this.transform = transform;
+        outputFile = new File(transformed);
+        tempDir = new File(FilenameUtils.concat(
+                jobConfig.getProperty("tmp.folder", "/tmp/"),
+                RandomStringUtils.randomAlphanumeric(IDENTIFIER_LENGTH)));
+        splitLogic = new SplitLogicFactory()
+                .getSplitter(
+                        checkParatmeterExist(jobConfig, "job." + id
+                                + ".splitStrategy"),
+                        checkParatmeterExist(jobConfig, "job." + id
+                                + ".splitProperty"));
+        exporterProperties = jobConfig;
+        splitInputFile();
+        return this;
+    }
+
+    public SplitJob configure(Properties jobConfig) throws Exception {
+        URI original = new URI(checkParatmeterExist(jobConfig, "job." + id
+                + ".source"));
+        URI transformed = new URI(checkParatmeterExist(jobConfig, "job." + id
+                + ".output"));
+        JSONArray transform = deserialize(new File(checkParatmeterExist(
+                jobConfig, "job." + id + ".transform")));
+        return this.configure(original, transform, transformed, jobConfig);
+    }
+
+    public void submit(MultiInstanceEngine engine) {
+
+        for (TransformTask t : chunks) {
+            t.setFuture(engine.transform(t.getInput(), transform,
+                    t.getOutput(), exporterProperties, this));
+        }
+    }
+
+    public void join() throws Exception {
+
+        for (TransformTask t : chunks) {
+            t.get();
+        }
+        reassambleOutput();
+    }
+
+    private void splitInputFile() throws IOException {
+        Files.createDirectories(new File(tempDir, "/out/").toPath());
+        chunks = splitLogic.splitFile(inputFile, tempDir);
+        taskCounter = chunks.size();
+    }
+
+    private void reassambleOutput() throws IOException {
+        if (exporterProperties.getProperty("format").equals("rdf"))
+            PartFilesReassembly.reconstructRDF(new File(tempDir, "/out/"),
+                    outputFile);
+        else if (exporterProperties.getProperty("format").equals("turtle"))
+            PartFilesReassembly.reconstructTurtle(new File(tempDir, "/out/"),
+                    outputFile);
+        else
+            PartFilesReassembly.reconstructLineBased(
+                    new File(tempDir, "/out/"), outputFile, true);
+        cleanup();
+    }
+
+    private JSONArray deserialize(File transform) throws IOException {
+        String transformStr = FileUtils.readFileToString(transform);
+        JSONArray jsonTransform = new JSONArray(transformStr);
+        return jsonTransform;
+    }
+
+    private String checkParatmeterExist(Properties prop, String key) {
+        String value = prop.getProperty(key);
+        if (value == null)
+            throw new IllegalArgumentException(
+                    "Missing required job property: " + key);
+        else
+            return value;
+    }
+
+    public String toString() {
+        return "[job [" + id + "] " + inputFile.getName();
+    }
+
+    @Override
+    public void done() {
+        taskCounter--;
+    }
+
+    @Override
+    public void failed(Exception ex) {
+        taskCounter--;
+    }
+
+    public void cleanup() {
+        FileUtils.deleteQuietly(tempDir);
+    }
+
+}
