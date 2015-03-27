3d124bc93ac0c816334e5770bd2055b3dbe1da9e
Andrey Bratus <andreybratus@yahoo.com>
2015-03-27 18:20:39 +0100

    docker image fix

--- docker/internal/p3-transformer-start.sh
+++ docker/internal/p3-transformer-start.sh
@@ -2,9 +2,12 @@
 
 BATCHREFINE_ROOT=/home/user/code/batchrefine
 
+echo "starting supervisor"
+supervisord
+
 VERSION=`cat ${BATCHREFINE_ROOT}/version`
 echo "transformer version is $VERSION"
 echo "transformer options are $@"
 
 # ... and the P3 transformer.
-java -jar ${BATCHREFINE_ROOT}/clients/clients-transformer/target/clients-transformer-${VERSION}-jar-with-dependencies.jar $@
+java -jar ${BATCHREFINE_ROOT}/clients/clients-transformer/target/clients-transformer-${VERSION}-jar-with-dependencies.jar $@ 2>&1 | tee /home/user/log/transformer.log
